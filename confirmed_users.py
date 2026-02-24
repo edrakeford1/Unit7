0b06bf3095b746941a4e12a98d043c0472de4b08
Elijah Drakeford <elijahdrakeford12@gmail.com>
2026-02-24 11:29:38 -0500

    init

---
+++ confirmed_users.py
@@ -0,0 +1,15 @@
+# start with users that need to be verified,
+# and an empty list to hold confirmed user
+unconfirmed_users = ['alice', 'brian', 'candace']
+confirmed_users = []
+
+# verify each user until there are no more unconfirmed users.
+# move each verified user into the list of confirmed users
+while unconfirmed_users:
+    current_user = unconfirmed_users.pop()
+
+    print(f"Verifying user: {current_user.title()}")
+    confirmed_users.append(current_user)
+
+
+
