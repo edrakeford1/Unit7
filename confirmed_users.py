5387b0110d2df23a7da23c7301b74dd2bee524ea
Elijah Drakeford <elijahdrakeford12@gmail.com>
2026-02-24 11:31:16 -0500

    adding print for confirmed users

--- confirmed_users.py
+++ confirmed_users.py
@@ -5,11 +5,17 @@ confirmed_users = []
 
 # verify each user until there are no more unconfirmed users.
 # move each verified user into the list of confirmed users
-while unconfirmed_users:
-    current_user = unconfirmed_users.pop()
+# while unconfirmed_users:
+#     current_user = unconfirmed_users.pop()
 
-    print(f"Verifying user: {current_user.title()}")
-    confirmed_users.append(current_user)
+#     print(f"Verifying user: {current_user.title()}")
+#     confirmed_users.append(current_user)
 
+for unconfirmed_user in unconfirmed_users:
+    print(f"Verifying user: {unconfirmed_user.title()}")
+    confirmed_users.append(unconfirmed_user)
 
-
+# display all confirmed user
+print("\nThe following users have been confirmed")
+for confirmed_user in confirmed_users:
+    print(confirmed_user.title())
